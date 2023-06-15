45162aeb022f0a311121a092f8f0a21f6fee8e66
Alessiovinci87 <alessio.vinci87@gmail.com>
2023-06-15 17:35:07 +0200

    Esercizio terminato

--- esercizio-dadi/js/scripts.js
+++ esercizio-dadi/js/scripts.js
@@ -6,4 +6,31 @@
 // Stampa il risultato nella console
 
 
+const playerScore = Math.floor(Math.random() * 6) + 1;
 
+
+const computerScore = Math.floor(Math.random() * 6) + 1;
+
+
+let winner;
+if (playerScore > computerScore) {
+  winner = "Giocatore";
+} else if (computerScore > playerScore) {
+  winner = "Computer";
+} else {
+  winner = "Pareggio";
+}
+
+
+const resultElement = document.getElementById("result");
+
+
+resultElement.innerHTML = `Punteggio del Giocatore: ${playerScore}<br><br>` +
+                          `Punteggio del Computer: ${computerScore}<br><br>` +
+                          `Vincitore: ${winner}`;
+
+
+
+console.log(`Punteggio del Giocatore: ${playerScore}`);
+console.log(`Punteggio del Computer: ${computerScore}`);
+console.log(`Vincitore: ${winner}`);
