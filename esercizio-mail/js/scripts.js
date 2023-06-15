76183cb6c9c6973df669adb2f4ab83c8e2bd333b
Alessiovinci87 <alessio.vinci87@gmail.com>
2023-06-15 16:24:41 +0200

    Inizializzazione js

---
+++ esercizio-mail/js/scripts.js
@@ -0,0 +1,13 @@
+// 1 - creare un form per chiedere la mail all'utente 
+// 2 - Selezionare gli elementi html con getElementById
+// 3 - Creare arroy con finti indirizzi email che verranno approvati
+// 4 - attivare "Submit" con "addEventListener" e aggiungere preventDefault per non inviare i dati al server.
+// 5 - Creare ciclo per stabilire quali mail saranno approvate
+// 6 - creare condizioni di approvazione
+// 7 - Svuotare la stringa per inserire nuovo indirizzo
+
+
+const accessForm = document.getElementById('access-form');
+const emailInput = document.getElementById('email');
+const resultMessage = document.getElementById('result-message');
+
