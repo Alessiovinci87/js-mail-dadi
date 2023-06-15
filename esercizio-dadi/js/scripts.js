da6b059f2c17818dad10dfa1351d522ae8d49743
Alessiovinci87 <alessio.vinci87@gmail.com>
2023-06-15 17:08:07 +0200

    Inizio esercizio dadi

---
+++ esercizio-dadi/js/scripts.js
@@ -0,0 +1,9 @@
+// Creare struttura html
+// Generare un numero casuale da 1 a 6 per il giocatore
+// Genera un numero casuale da 1 a 6 per il computer
+// Creare ciclo per determinare il vincitore
+// Rendere visibile in pagina il risultato 
+// Stampa il risultato nella console
+
+
+
